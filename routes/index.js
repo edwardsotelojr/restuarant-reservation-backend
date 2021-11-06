6c43253a65320480e2b87a31ae062c94dc218469
Sir Edward <edwardsotelojr@gmail.com>
2021-11-06 02:47:40 -0500

    sign up http request done. test with Postman. need more work and validation parameters

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,7 @@
-//lol
+const { Router } = require('express');
+const router = Router();
+const { signup } = require('../controllers/user.js'); 
+
+router.post('/signup', signup);
+
+module.exports = router;

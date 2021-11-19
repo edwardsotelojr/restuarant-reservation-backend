83a1bee69bf6470ed73895b9fc1abc585a441b20
Sir Edward <edwardsotelojr@gmail.com>
2021-11-18 23:20:36 -0600

    able to get what tables are taken at a time n date

--- controllers/Reservation.js
+++ controllers/Reservation.js
@@ -7,6 +7,12 @@ exports.getReservations = async (req, res) => {
     })
 }
 
+validateReservationRequest = (r) => {
+    if(r.name.length < 3){
+        return {error: true, msg: "name too short"}
+    }
+}
+
 exports.setReservation = async (req, res) => {
     const {
         name, diners, tables, time, date
@@ -14,6 +20,10 @@ exports.setReservation = async (req, res) => {
  
     var r = new Reservation({name: name, diners: diners,
          tables: tables, time: time, date: date})
+    let ready = validateReservationRequest(r);
+    if(ready.error == true) {
+        return res.status(404).json({msg: error.msg})
+    }
     r.save(
         function(err, re) {
             console.log(err)
@@ -21,4 +31,19 @@ exports.setReservation = async (req, res) => {
         if (err) return res.status(400),json({err: err});
         return res.status(200).json({reservation: re})
     })
+}
+
+exports.getAvailableTables = async (req, res) => {
+    const { time, date } = req.body
+    Reservation.find({$and: [{time: time}, {date: date}]})
+    .then(r => {
+        var tables = []
+        r.map(rr => rr.tables.map(t => tables.push(t)))
+        let tabless = tables.filter((t, index) => {
+            return tables.indexOf(t) === index
+        })
+        console.log(tabless)
+        return res.status(200).json({msg: ""})
+    })
+    
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const router = Router();
 const {signup, login} = require('../controllers/user.js'); 
-const {setReservation} = require('../controllers/reservation.js')
+const {setReservation, getAvailableTables} = require('../controllers/reservation.js')
 router.post('/setReservation', setReservation);
+router.get('/getAvailableTables', getAvailableTables);
+
 router.post('/signup', signup);
 router.post('/login', login)
 module.exports = router;

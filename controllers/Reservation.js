0fb11518ead7a489ec07a6c52cea3f3ae8d24c7a
Sir Edward <edwardsotelojr@gmail.com>
2021-11-27 00:05:04 -0600

    get taken tables. Range selected time-past 75 mins

--- controllers/Reservation.js
+++ controllers/Reservation.js
@@ -34,16 +34,53 @@ exports.setReservation = async (req, res) => {
 }
 
 exports.getAvailableTables = async (req, res) => {
-    const { time, date } = req.body
-    Reservation.find({$and: [{time: time}, {date: date}]})
+    //console.log(req)
+    console.log("here")
+    var timeBefore = ["", "", "", "", ""] // Array of time before selectedTime. interval of 15 minutes
+    const { time, date } = req.query
+    console.log(typeof(time))
+    var hour
+    var minute
+    for(var i = 0; i < 5; i++){
+        if(i == 0){
+            if(parseInt(time.substring(2)) < 15 ){ // time is X:14 or less
+                hour = parseInt(time.substring(0,1)) - 1
+                minute = 60 - (15 - parseInt(time.substring(2)))
+                timeBefore[0] = (hour).toString() + ":" + minute.toString()
+            }else{
+                timeBefore[0] = hour.toString() + ":" + (parseInt(time.substring(2))-15).toString()
+            }
+        }else{ 
+            if(parseInt(timeBefore[i-1].substring(2)) < 15 ){ // 15 before is in the previous hour
+                hour = parseInt(timeBefore[i-1].substring(0,1)) - 1
+                minute = 60 - (15 - parseInt(timeBefore[i-1].substring(2)))
+                timeBefore[i] = (hour).toString() + ":" + minute.toString()
+            }else{
+                timeBefore[i] = hour.toString() + ":" + (parseInt(timeBefore[i-1].substring(2))-15).toString()
+            }
+        }
+        if(timeBefore[i].length == 3){
+            timeBefore[i] = timeBefore[i] + "0"
+        }
+        console.log(timeBefore[i])
+    }
+    console.log("time: ", time)
+    console.log("date: ", date)
+
+    timeBefore.forEach(element => {
+        console.log(element)
+    });
+    Reservation.find({$and: [{time: [time, timeBefore[0], timeBefore[1],
+        timeBefore[2], timeBefore[3], timeBefore[4]]}, {date: date}]})
     .then(r => {
+        console.log(r)
         var tables = []
         r.map(rr => rr.tables.map(t => tables.push(t)))
         let tabless = tables.filter((t, index) => {
             return tables.indexOf(t) === index
         })
-        console.log(tabless)
-        return res.status(200).json({msg: ""})
+        console.log(tables)
+        return res.status(200).json({reservation: tables})
     })
     
 }

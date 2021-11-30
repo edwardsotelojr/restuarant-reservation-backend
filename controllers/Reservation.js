9ea8fa4f30ce7978be2d7eb89afed02219f8429d
Sir Edward <edwardsotelojr@gmail.com>
2021-11-29 23:09:35 -0600

    made changes to setReservation

--- controllers/Reservation.js
+++ controllers/Reservation.js
@@ -33,36 +33,57 @@ exports.setReservation = async (req, res) => {
     })
 }
 
+ isNumeric = (num) => {
+    return !isNaN(num)
+  }
+ 
 exports.getAvailableTables = async (req, res) => {
     //console.log(req)
     console.log("here")
     var timeBefore = ["", "", "", "", ""] // Array of time before selectedTime. interval of 15 minutes
-    const { time, date } = req.query
-    console.log(typeof(time))
+    var { time, date } = req.query
+    var lastTwo = time.slice(-2)
     var hour
     var minute
     for(var i = 0; i < 5; i++){
-        if(i == 0){
-            if(parseInt(time.substring(2)) < 15 ){ // time is X:14 or less
-                hour = parseInt(time.substring(0,1)) - 1
-                minute = 60 - (15 - parseInt(time.substring(2)))
-                timeBefore[0] = (hour).toString() + ":" + minute.toString()
+        if(i == 0){ // initial time
+            if(isNumeric(time.substring(0,2))){ // hour is 10, 11, or 12.
+                hour = parseInt(time.substring(0,2)) 
+                minute = parseInt(time.substring(3,5))
             }else{
-                timeBefore[0] = hour.toString() + ":" + (parseInt(time.substring(2))-15).toString()
+                hour = parseInt(time.substring(0,1)) 
+                minute = parseInt(time.substring(2,4))
+            }
+            if(parseInt(time.substring(2,4)) < 15 ){ // time is X:14 or less
+                hour = hour - 1
+                minute = 60 - (15 - minute)
+                timeBefore[0] = hour.toString() + ":" + minute.toString()
+            }else{
+                minute = minute - 15
+                timeBefore[0] = hour.toString() + ":" + minute.toString()
             }
         }else{ 
-            if(parseInt(timeBefore[i-1].substring(2)) < 15 ){ // 15 before is in the previous hour
-                hour = parseInt(timeBefore[i-1].substring(0,1)) - 1
-                minute = 60 - (15 - parseInt(timeBefore[i-1].substring(2)))
+            if(isNumeric(timeBefore[i-1].substring(0,2))){ // hour is 10, 11, or 12.
+                hour = parseInt(timeBefore[i-1].substring(0,2)) 
+                minute = parseInt(timeBefore[i-1].substring(3,5))
+            }else{
+                hour = parseInt(timeBefore[i-1].substring(0,1)) 
+                minute = parseInt(timeBefore[i-1].substring(2,4))
+            }
+            if(minute < 15 ){ // 15 before is in the previous hour
+                hour = hour - 1
+                minute = 60 - (15 - minute)
                 timeBefore[i] = (hour).toString() + ":" + minute.toString()
             }else{
-                timeBefore[i] = hour.toString() + ":" + (parseInt(timeBefore[i-1].substring(2))-15).toString()
+                minute = minute - 15
+                timeBefore[i] = hour.toString() + ":" + minute.toString()
             }
         }
         if(timeBefore[i].length == 3){
             timeBefore[i] = timeBefore[i] + "0"
         }
         console.log(timeBefore[i])
+        timeBefore[i] = timeBefore[i] + " " + lastTwo
     }
     console.log("time: ", time)
     console.log("date: ", date)
